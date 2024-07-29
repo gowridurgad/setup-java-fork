46ef1defef4364d51bb2e83835b92e00e190fcc1
gowridurgad <gowridurga@github.com>
2024-07-29 16:48:16 +0530

    apidata

--- src/distributions/sapmachine/installer.ts
+++ src/distributions/sapmachine/installer.ts
@@ -73,11 +73,6 @@ export class SapMachineDistribution extends JavaBase {
       );
     }
 
-    core.info(`fetchedreleaseapidata: ${JSON.stringify(fetchedReleasesJson)}`);
-    console.log(
-      `Fetched release API data: ${JSON.stringify(fetchedReleasesJson)}`
-    );
-
     if (!fetchedReleasesJson) {
       throw new Error(
         `Couldn't fetch SapMachine versions information from both primary and backup urls`
